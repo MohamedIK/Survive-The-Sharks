60eaf2119906c3c07ff0d6543c2da442db356720
M <mois20485@gmail.com>
2023-05-18 22:38:13 +0300

    New changes
    -Adjusted initial position of fish and shark
    -Added controls for fish for going up and down
    -Added fish control velocity and speed

--- Scripts/Fish.gd
+++ Scripts/Fish.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+signal hit
+
 @export var speed = 400
 var screen_size
 
@@ -13,12 +15,6 @@ func _process(delta):
 		velocity.y += 1
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
-
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
-	else:
-		$AnimatedSprite2D.stop()
 	
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
