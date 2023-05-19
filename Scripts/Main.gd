3541558eb83f996b9ca9f6ec4fbcdc2d72bbfad4
M <mois20485@gmail.com>
2023-05-19 11:36:22 +0300

    New code changes

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -2,3 +2,13 @@ extends Node
 
 @export var mob_scene: PackedScene
 var score
+
+
+func game_over():
+	$"Score Timer".stop()
+
+
+func new_game():
+	score = 0
+	$Fish.start($"Start Position".position)
+	$"Start Timer".start()
